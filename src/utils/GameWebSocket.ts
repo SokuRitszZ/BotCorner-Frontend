b26bc8d55cff955fb66a707e35a22a2f2311690d
Andrew Leung <1062708758@qq.com>
2022-12-24 10:52:13 +0800

    design PubSub-GameWebSocket

---
+++ src/utils/GameWebSocket.ts
@@ -0,0 +1,67 @@
+type IActionMessage = {
+  action: string;
+  data: any;
+};
+
+type ICallback = {
+  action: string | string[];
+  callback: (data: any) => void;
+};
+
+class GameWebSocket {
+  private server: WebSocket;
+  private callbackMap: {[key: string]: Function[]};
+
+  constructor(url: string) {
+    this.server = new WebSocket(url);
+    this.callbackMap = {};
+
+    this.server.onopen = () => window._alert("success", "WebSocket连接成功");
+    this.server.onclose = () => window._alert("primary", "WebSocket已关闭");
+    this.server.onerror = (error) => window._alert("danger", `WebSocket出错：${error}`);
+    this.server.onmessage = (message) => this.callback(JSON.parse(message.data)); ;
+  }
+
+  /**
+   * 订阅者 
+   * @param {ICallback} callback 
+   * @returns 
+   */
+  public addCallback(callback: ICallback) {
+    const action = callback.action;
+    if (action instanceof Array) {
+      action.forEach(action => this.addCallback({
+        action, 
+        callback: callback.callback
+      }))
+    } else {
+      if (!this.callbackMap[action]) 
+      this.callbackMap[action] = [];
+      this.callbackMap[callback.action as string].push(callback.callback);
+    }
+    return this;
+  }
+
+  /**
+   * 发布者 (本来是设置成private，但有可能信息不通过ws传过来，需要手动发布信息，因此设置成public)
+   * @param {IActionMessage} message 
+   */
+  public callback(message: IActionMessage) {
+    const action = message.action;
+    if (this.callbackMap[action]) {
+      this.callbackMap[action].forEach(fn => fn(message.data));
+    } else {
+      window._alert("danger", `执行回调失败：不存在${message.action}`);
+    }
+  }
+
+  public sendMessage(message: IActionMessage) {
+    this.server.send(JSON.stringify(message));
+  }
+
+  public close() {
+    this.server.close();
+  }
+}
+
+export default GameWebSocket;
