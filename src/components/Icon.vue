e8a5c48dd89f3e1b70bc375c2a26b2f6afe298be
Andrew Leung <1062708758@qq.com>
2023-01-04 00:00:28 +0800

    update component Icon

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -49,6 +49,21 @@
       <path
         d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
     </svg>
+    <svg v-else-if="type === 'pause'" xmlns="http://www.w3.org/2000/svg" :width="props.size || 16" :height="props.size || 16" fill="currentColor" class="bi bi-pause-circle-fill"
+      viewBox="0 0 16 16">
+      <path
+        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z" />
+    </svg>
+    <svg v-else-if="type === 'skip'" xmlns="http://www.w3.org/2000/svg" :width="props.size || 16" :height="props.size || 16" fill="currentColor" class="bi bi-skip-forward-fill"
+      viewBox="0 0 16 16">
+      <path
+        d="M15.5 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V8.753l-6.267 3.636c-.54.313-1.233-.066-1.233-.697v-2.94l-6.267 3.636C.693 12.703 0 12.324 0 11.693V4.308c0-.63.693-1.01 1.233-.696L7.5 7.248v-2.94c0-.63.693-1.01 1.233-.696L15 7.248V4a.5.5 0 0 1 .5-.5z" />
+    </svg>
+    <svg v-else-if="type === 'skip-rev'" xmlns="http://www.w3.org/2000/svg" :width="props.size || 16" :height="props.size || 16" fill="currentColor" class="bi bi-skip-backward-fill"
+      viewBox="0 0 16 16">
+      <path
+        d="M.5 3.5A.5.5 0 0 0 0 4v8a.5.5 0 0 0 1 0V8.753l6.267 3.636c.54.313 1.233-.066 1.233-.697v-2.94l6.267 3.636c.54.314 1.233-.065 1.233-.696V4.308c0-.63-.693-1.01-1.233-.696L8.5 7.248v-2.94c0-.63-.692-1.01-1.233-.696L1 7.248V4a.5.5 0 0 0-.5-.5z" />
+    </svg>
   </div>
 </template>
 
