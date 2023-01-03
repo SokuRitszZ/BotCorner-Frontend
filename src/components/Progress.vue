e1b16f79b564366befc66fcf6e430c6dabee5475
Andrew Leung <1062708758@qq.com>
2023-01-03 23:57:46 +0800

    progress component

---
+++ src/components/Progress.vue
@@ -0,0 +1,112 @@
+<script setup lang="ts">
+import { nextTick, onMounted, ref, watch } from 'vue';
+
+type PropsType = {
+  max: number
+  width: number
+  modelValue: number
+  class?: string
+};
+const props = defineProps<PropsType>();
+const current_position = ref<number>(0);
+const $progress = ref<HTMLDivElement>();
+const $button = ref<HTMLDivElement>();
+const $line = ref<HTMLDivElement>();
+const emit = defineEmits(["change", "update:modelValue"]);
+const max = ref<number>(0);
+const shouldEdit = ref<boolean>(false);
+
+const setButtonPosition = (newV: number) => {
+  if (!$button.value) return ;
+  $button.value.style.setProperty('--moved', `${newV / props.max * props.width}px`);
+}
+
+watch(current_position, (newV) => {
+  newV = Math.min(newV, props.max);
+  newV = Math.max(0, newV);
+  current_position.value = newV;
+  setButtonPosition(newV);
+});
+
+watch(() => props.modelValue, (newV) => {
+  current_position.value = newV;
+  emit("update:modelValue", newV);
+});
+
+onMounted(async () => {
+  await nextTick();
+  max.value = Math.max(props.max, props.width);
+  $line.value?.style.setProperty('width', `${props.width}px`);
+});
+
+const shouldMove = ref<boolean>(false);
+
+const oldV = ref<number>(0);
+
+const handleMousedown = (e: MouseEvent) => {
+  shouldMove.value = true;
+  oldV.value = current_position.value;
+};
+
+window.addEventListener("mousemove", (e) => {
+  if (shouldMove.value) {
+    const offsetX = $progress.value?.getBoundingClientRect().left;
+    let newV = Math.floor((e.clientX - offsetX!) / (props.width / props.max));
+
+    current_position.value = newV;
+  }
+});
+
+window.addEventListener("mouseup", (e) => {
+  if (!shouldMove.value) return ;
+  shouldMove.value = false;
+  emit("change", current_position.value, oldV.value);
+});
+
+const $input = ref<HTMLInputElement>();
+const edit = () => {
+  shouldEdit.value = true;
+  oldV.value = current_position.value;
+  if (!$input.value) return ;
+  $input.value.value = current_position.value.toString();
+  $input.value!.focus();
+};
+
+const okEdit = () => {
+  shouldEdit.value = false;
+  if (!$input.value) return ;
+  const value = parseInt($input.value.value);
+  if (isNaN(value)) return ;
+  current_position.value = value;
+  emit("change", current_position.value, oldV.value);
+}
+</script>
+
+<template>
+  <div :class="props.class" class="select-none w-fit m-auto pr-4 rounded-full relative flex items-center gap-1">
+    <div v-if="!shouldEdit" @click="edit" class="w-24 mr-3 flex justify-end">{{ current_position }} / {{ props.max }}</div>
+    <input v-else @keyup.enter="okEdit" @blur="okEdit" ref="$input" type="number" class="w-24 mr-3 bg-purple-400 rounded-xl px-3">
+    <div ref="$progress" class="w-fit flex items-center">
+      <div ref="$line" class="bg-purple-900 h-1 rounded-full" />
+      <div @mousedown="handleMousedown" id="button" ref="$button" class="cursor-pointer w-[14px] h-[14px] bg-purple-700 rounded-full absolute" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+#button {
+  --moved: 0px;
+  transform: translateX(
+    calc(var(--moved) - 7px)
+  );
+  transition: .2s;
+}
+
+#button:active {
+  transition: none;
+}
+
+input::-webkit-inner-spin-button {
+  display: none;
+}
+</style>
--- src/pages/Lab.vue
+++ src/pages/Lab.vue
@@ -1,40 +1,51 @@
 <script setup lang="ts">
 import useGameStore from '@/store/gameStore';
 import { onMounted, ref } from 'vue';
+import Progress from '@/components/Progress.vue';
 
 const $parent = ref();
 const $canvas = ref();
 const gameStore = useGameStore();
-
-const prepare = (options: {
-  mode: "single" | "multi" | "record",
-  initData: any;
-}) => {
-  if ($canvas.value) $canvas.value.remove();
-  $canvas.value = document.createElement("canvas");
-  $parent.value.append($canvas.value);
-  gameStore.createGame("gomoku", $parent.value, $canvas.value);
-  gameStore.game
-    ?.prepare({
-      mode: options.mode,
-      initData: options.initData,
-    })
-    .start();
-};
+const val = ref<number>(0);
 
 onMounted(() => {
-  prepare({
-    mode: 'single',
-    initData: undefined
-  });
-});
+  // setInterval(() => {
+  //   // val.value++;
+  // }, 10);
+})
+
+
+// const prepare = (options: {
+//   mode: "single" | "multi" | "record",
+//   initData: any;
+// }) => {
+//   if ($canvas.value) $canvas.value.remove();
+//   $canvas.value = document.createElement("canvas");
+//   $parent.value.append($canvas.value);
+//   gameStore.createGame("gomoku", $parent.value, $canvas.value);
+//   gameStore.game
+//     ?.prepare({
+//       mode: options.mode,
+//       initData: options.initData,
+//     })
+//     .start();
+// };
+
+// onMounted(() => {
+//   prepare({
+//     mode: 'single',
+//     initData: undefined
+//   });
+// });
+const handleChange = (data: number) => console.log(data);
 </script>
 
 <template>
-  <div class="mt-24 h-full">
-    <div ref="$parent" class="bg-black m-auto w-2/3 h-3/4">
+  <div class="mt-24 h-full text-center">
+    <!-- <div ref="$parent" class="bg-black m-auto w-2/3 h-3/4">
       <canvas ref="$canvas"></canvas>
-    </div>
+    </div> -->
+    <Progress @change="handleChange" v-model="val" :max="200" :width="100" />
   </div>
 </template>
 
