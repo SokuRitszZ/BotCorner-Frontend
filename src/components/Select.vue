5ebce2e36f8b66c043232d2c0f2fdf54c32d8374
Andrew Leung <1062708758@qq.com>
2023-01-04 15:53:56 +0800

    fix component Select

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -9,7 +9,7 @@
         d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
     </svg>
     <Transition>
-      <div v-if="status === 'open'" class="rounded-xl absolute bg-white w-full min-h-[50px] max-h-72 left-0 top-0 translate-y-11 overflow-scroll">
+      <div v-if="status === 'open'" class="rounded-xl absolute bg-white w-full min-h-[50px] max-h-72 left-0 top-0 translate-y-11 overflow-scroll z-50 border-[1px]">
         <div @click="select(item)" v-for="item in list" :key="item.value" class="hover:bg-gray-200 p-3">{{ item.key }}</div>
       </div>
     </Transition>
