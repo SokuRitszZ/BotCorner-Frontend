a008fa5a32d0dbac70dac9e1135ed5c9982ea7df
Andrew Leung <1062708758@qq.com>
2023-01-07 14:50:09 +0800

    design my ugly website symbol: BotCorner

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,14 @@
 <template>  
-  <nav class="fixed top-0 left-0 w-screen py-5 bg-gray-700 z-10">
+  <nav class="fixed top-0 leet-0 w-screen min-w-[1600px] py-5 bg-gray-700 z-10">
     <div class="m-auto max-w-7xl px-10 relative">
       <div class="flex justify-between">
         <div class="flex items-center justify-start gap-10 relative">
           <div class="icon">
-            <img class="block h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-            alt="Your Company">
+            <Icon type="botcorner" :size="32" />
           </div>
           <div class="nav_s flex justify-start gap-3 self-start">
             <RouterLink class="transition font-bold text-white py-1 px-3 rounded-lg hover:bg-gray-900 active:900" v-for="url in urls" :to="url.path">{{ url.name }}</RouterLink>
+            <a class="transition font-bold text-white py-1 px-3 rounded-lg hover:bg-gray-900 active:900" href="https://www.yuque.com/sokuritszz/upyv68?# 《开放文档》" target="_blank">外链：说明</a>
           </div>
         </div>
         <div>
@@ -22,6 +22,7 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
 import Profile from './Profile.vue';
+import Icon from './Icon.vue';
 
 export type IUrl = {
   path: string
--- src/pages/Lab.vue
+++ src/pages/Lab.vue
@@ -4,6 +4,7 @@ import { onMounted, ref } from 'vue';
 import Progress from '@/components/Progress.vue';
 import Window from '@/components/SokuWindow.vue';
 import ChatRoom from '@/components/ChatRoom.vue';
+import Icon from '@/components/Icon.vue';
 
 const $parent = ref();
 const $canvas = ref();
@@ -51,8 +52,12 @@ const handleChange = (data: number) => console.log(data);
     <Progress @change="handleChange" v-model="val" :max="200" :width="100" />
     <Window ref="$window"></Window>
     <ChatRoom />
+    <div class="w-40 h-40 bg-purple-400" />
+    <Icon type="botcorner" :size="240"/>
+    <svg width="32" height="32" viewBox="0 0 8 8"><circle cx="2" cy="2" r="2" fill="#c33"></circle><circle cx="6" cy="2" r="2" fill="#33c"></circle><circle cx="4" cy="5.5" r="2" fill="#3c3"></circle></svg>
   </div>
 </template>
 
 <style scoped lang="scss">
+
 </style>
