f398d03a75f700bd746e01d6f2b26a894d691ec2
Andrew Leung <1062708758@qq.com>
2023-01-24 00:10:56 +0800

    replace with new component on page GameLobby

--- src/pages/GameLobby.vue
+++ src/pages/GameLobby.vue
@@ -1,30 +1,62 @@
 <template>
   <div class="lg:normal h-screen w-screen bg-purple-500 overflow-auto flex flex-col justify-start">
     <!-- card container -->
-    <Slider ref="$slider" class="w-[500px] h-[600px] m-auto overflow-hidden shadow-inner shadow-purple-500" />
-    <div class="text-center mb-10 relative mx-auto">
-      <nav class="bg-gray-700 opacity-60 flex justify-between gap-3 p-3 rounded-full">
-        <div @click="$slider.slideTo(index)" v-for="(game, index) in games" :class="{'selected': index === $slider.ptr }"
-          class="hover:bg-purple-600 p-2 rounded-full text-2xl cursor-pointer transition">
-          {{ game.name }}
-        </div>
-      </nav>
-    </div>
+    <NewSlider v-model="current" ref="$slider" :width="500" :height="600" class="m-auto"/>
+    <!-- <Slider ref="$slider" class="w-[500px] h-[600px] m-auto overflow-hidden shadow-inner shadow-purple-500" /> -->
+    <Bar @click="handleClickBar" v-model="current" :list="bar_list" class="w-fit m-auto font-semibold text-xl" />
   </div>
 </template>
 
 <script setup lang="ts">
-import Slider from '@/components/Slider.vue';
+import Bar from '@/components/Bar.vue';
+import NewSlider from '@/components/NewSlider.vue';
 import useCacheStore, { IGame } from '@/store/cacheStore';
 import { onMounted, ref } from 'vue';
 
 const games = ref<IGame[]>([]);
 const cacheStore = useCacheStore();
 const $slider = ref();
+const bar_list = ref<string[]>([]);
+const current = ref<number>(0);
 
 onMounted(() => {
-  cacheStore.getGames()
-    .then(list => games.value = list);
+  cacheStore.getGames
+    .then(list => {
+      games.value = list;
+      bar_list.value = list.map(x => x.name);
+      current.value = Math.floor(Math.random() * list.length);
+      start();
+    });
+});
+
+const timer = ref<NodeJS.Timer>();
+
+function start(): void {
+  clearTimeout(timer.value);
+  function act() {
+    timer.value = setTimeout(() => {
+      current.value = (current.value + 1) % games.value.length;
+      act();
+    }, 3000);
+  }
+  act();
+}
+
+function stop() {
+  clearTimeout(timer.value);
+}
+
+function handleClickBar() {
+  clearTimeout(timer.value);
+  start();
+}
+
+function autoSlide() {
+  document.visibilityState === "hidden" && stop() || start();
+}
+
+onMounted(() => {
+  document.addEventListener("visibilitychange", autoSlide)
 });
 
 </script>
