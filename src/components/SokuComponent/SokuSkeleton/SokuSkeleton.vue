fbbcc1d49d1609ce435500db2ddaa44bacbd4ee3
Andrew Leung <1062708758@qq.com>
2023-03-10 10:53:53 +0800

    feat:Soku-Skeleton

---
+++ src/components/SokuComponent/SokuSkeleton/SokuSkeleton.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+type PropsType = {
+  class?: string[] | string;
+  isLoading: boolean;
+};
+const props = defineProps<PropsType>();
+const dom = ref<HTMLDivElement>();
+
+</script>
+
+<template>
+  <div
+    ref="dom"
+    class="--soku-skeleton"
+    :class="[props.class, (props.isLoading && 'loading') || '']"
+  >
+  <transition>
+    <slot v-if="!props.isLoading"></slot>
+  </transition>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import url('./style.scss');
+
+.v-enter-active,
+.v-leave-active {
+  transition: .2s;
+}
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+.v-enter-to,
+.v-leave-from {
+  opacity: 1;
+}
+</style>
