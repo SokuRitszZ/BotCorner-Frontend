0a68e34882a83d103e4b3f3aeddc5d52040ffc15
Andrew Leung <1062708758@qq.com>
2023-03-12 18:21:45 +0800

    feat:use account api

--- src/pages/WelcomeView/WelcomeView.vue
+++ src/pages/WelcomeView/WelcomeView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import SokuModal from '@/components/SokuComponent/SokuModal/SokuModal.vue';
 import SokuImgSkeleton from '@/components/SokuComponent/SokuSkeleton/SokuImgSkeleton.vue';
 import SokuSkeleton from '@/components/SokuComponent/SokuSkeleton/SokuSkeleton.vue';
 import useUserStore from '@/store/userStore';
@@ -21,9 +22,89 @@ onMounted(() => {
     testLoading.value = false;
   }, 3000);
 });
+
+const modalRegister = ref();
+function toRegister() {
+  modalRegister.value!.show();
+}
+const payloadRegister = ref<{
+  username: string;
+  password: string;
+  confirmed_password: string;
+}>({
+  username: '',
+  password: '',
+  confirmed_password: '',
+});
+const isRegistering = ref(false);
+async function submitRegister(e: Event) {
+  e.preventDefault();
+  try {
+    isRegistering.value = true;
+    await userStore.register(payloadRegister.value);
+    modalRegister.value.hide();
+  } finally {
+    isRegistering.value = false;
+  }
+}
+
+const modalLogin = ref();
+function toLogin() {
+  modalLogin.value!.show();
+}
+const payloadLogin = ref<{
+  username: string;
+  password: string;
+}>({
+  username: '',
+  password: '',
+});
+const isLoging = ref(false);
+async function submitLogin(e: Event) {
+  e.preventDefault();
+  try {
+    isLoging.value = true;
+    await userStore.getTokenByApi(payloadLogin.value);
+    modalLogin.value.hide();
+  } finally {
+    isLoging.value = false;
+  }
+}
 </script>
 
 <template>
+  <SokuModal ref="modalRegister" title="注册BotCorner账号" class="w-[300px]">
+    <form @submit="submitRegister" class="form-register">
+      <label for="username">用户名</label>
+      <input v-model="payloadRegister.username" name="username" type="text" />
+      <label for="password">密码</label>
+      <input
+        v-model="payloadRegister.password"
+        name="password"
+        type="password"
+      />
+      <label for="confirmedPassword">确认密码</label>
+      <input
+        v-model="payloadRegister.confirmed_password"
+        name="confirmedPassword"
+        type="password"
+      />
+      <button :disabled="isRegistering" class="submit-btn">
+        {{ !isRegistering ? '提交' : '提交中' }}
+      </button>
+    </form>
+  </SokuModal>
+  <SokuModal ref="modalLogin" title="登录BotCorner" class="w-[300px]">
+    <form @submit="submitLogin" class="form-login">
+      <label for="username">用户名</label>
+      <input v-model="payloadLogin.username" name="username" type="text" />
+      <label for="password">密码</label>
+      <input v-model="payloadLogin.password" name="password" type="password" />
+      <button :disabled="isLoging" class="submit-btn">
+        {{ !isLoging ? '提交' : '提交中' }}
+      </button>
+    </form>
+  </SokuModal>
   <div class="welcome-view w-full h-full">
     <header class="announce">
       <div class="title">公告</div>
@@ -34,15 +115,18 @@ onMounted(() => {
       </div>
     </header>
     <div class="welcome-board">
-      <SokuImgSkeleton class="title-icon" url="https://sdfsdf.dev/100x100.png,123123,321321"/>
+      <SokuImgSkeleton
+        class="title-icon"
+        url="https://sdfsdf.dev/100x100.png,123123,321321"
+      />
       <div class="title">
         <template v-if="!isLogin"> 欢迎来到BotCorner！ </template>
-        <template v-else> 你好，{{ name }} </template>
+        <template v-else> 你好，{{ name }}! </template>
       </div>
       <div class="options">
         <template v-if="!isLogin">
-          <button class="option">前往注册</button>
-          <button class="option">现在登录</button>
+          <button @click="toRegister" class="option">前往注册</button>
+          <button @click="toLogin" class="option">现在登录</button>
         </template>
         <template v-else>
           <button class="option">游戏大厅</button>
@@ -52,23 +136,11 @@ onMounted(() => {
     </div>
     <main class="main">
       <div class="new-record-list">
-        <header class="title">最近比赛</header>
+        <header class="title">公告栏</header>
         <main class="main">
           <SokuSkeleton :is-loading="testLoading">
-            <div class="record-card">
-              <h1 class="font-bold text-3xl text-center pt-4">盘蛇</h1>
-              <div class="avatars">
-                <img
-                  class="avatar"
-                  src="https://sdfsdf.dev/100x100.png"
-                  alt=""
-                />
-                <img
-                  class="avatar"
-                  src="https://sdfsdf.dev/100x100.png"
-                  alt=""
-                />
-              </div>
+            <div class="one-announce">
+              <h1 class="title">版本 1.0.0 更新公告</h1>
               <div class="time">
                 {{ dayjs().format('YYYY-MM-DD hh:mm:ss') }}
               </div>
