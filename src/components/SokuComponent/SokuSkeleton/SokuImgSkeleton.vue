3c2d0ef384efecca4406e0f0a1db3d409720ecea
Andrew Leung <1062708758@qq.com>
2023-03-12 09:15:11 +0800

    feat:SokuImgSkeleton

---
+++ src/components/SokuComponent/SokuSkeleton/SokuImgSkeleton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import SokuSkeleton from './SokuSkeleton.vue';
+
+type PropsType = {
+  class?: string | string[];
+  url: string;
+};
+const props = defineProps<PropsType>();
+
+const isLoading = ref(true);
+const domImg = ref<HTMLImageElement>();
+onMounted(() => {
+  domImg.value!.addEventListener('load', () => {
+    isLoading.value = false;
+  });
+})
+</script>
+
+<template>
+  <SokuSkeleton :class="props.class" :is-loading="isLoading">
+    <img ref="domImg" class="w-full h-full" :src="props.url" alt="">
+  </SokuSkeleton>
+</template>
