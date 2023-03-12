23aa071484980e839c6005cff4e805cd6818e6fb
Andrew Leung <1062708758@qq.com>
2023-03-12 19:23:07 +0800

    feat:use ratings api

--- src/pages/GameRating/RatingView.vue
+++ src/pages/GameRating/RatingView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import SokuImgSkeleton from '@/components/SokuComponent/SokuSkeleton/SokuImgSkeleton.vue';
-import useCacheStore, { IGame } from '@/store/cacheStore';
+import useCacheStore, { IGame, IRating } from '@/store/cacheStore';
 import leftpad from '@/utils/leftpad';
 import { faker } from '@faker-js/faker';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 
 const cacheStore = useCacheStore();
 
@@ -11,6 +11,15 @@ const games = ref<IGame[]>([]);
 onMounted(async () => {
   games.value = await cacheStore.getGames;
 });
+
+const ratings = ref<IRating[]>([]);
+
+const gameCur = ref();
+
+watch(gameCur, async (newV) => {
+  ratings.value = (await cacheStore.getRatings(newV));
+});
+
 </script>
 
 <template>
@@ -20,40 +29,45 @@ onMounted(async () => {
         <input class="search" type="text" placeholder="搜寻用户（id / 模糊名字）">
         <div class="options">
           <div v-for="game in games" :key="game.id">
-            <input :id="game.title" :value="game.id" name="game" type="radio">
+            <input v-model="gameCur" :id="game.title" :value="game.id" name="game" type="radio">
             <label :for="game.title">{{ game.name }}</label>
           </div>
         </div>
       </form>
     </div>
-    <div class="-container rating-list">
+    <div v-if="ratings.length > 0" class="-container rating-list">
       <div class="top-3">
-        <div class="medal runner-up">
-          <SokuImgSkeleton class="avatar" url="http://sdfsdf.dev/100x100.png,223344,334455" />
-          <span>{{ faker.name.fullName() }}</span>
-          <span class="text-4xl">{{ Math.random() * 10000 >>> 0 }}</span>
+        <div v-if="ratings.length > 0" class="medal runner-up">
+          <SokuImgSkeleton class="avatar" :url="ratings[0].avatar" />
+          <span>{{ ratings[0].username }}</span>
+          <span class="text-4xl">{{ ratings[0].score }}</span>
         </div>
-        <div class="medal champion">
-          <SokuImgSkeleton class="avatar" url="http://sdfsdf.dev/100x100.png,556677,667788" />
-          <span>{{ faker.name.fullName() }}</span>
-          <span class="text-4xl">{{ Math.random() * 10000 >>> 0 }}</span>
+        <div v-if="ratings.length > 1" class="medal champion">
+          <SokuImgSkeleton class="avatar" :url="ratings[1].avatar" />
+          <span>{{ ratings[1].username }}</span>
+          <span class="text-4xl">{{ ratings[1].score }}</span>
         </div>
-        <div class="medal third-place">
-          <SokuImgSkeleton class="avatar" url="http://sdfsdf.dev/100x100.png,99aabb,aabbcc" />
-          <span>{{ faker.name.fullName() }}</span>
-          <span class="text-4xl">{{ Math.random() * 10000 >>> 0 }}</span>
+        <div v-if="ratings.length > 2" class="medal third-place">
+          <SokuImgSkeleton class="avatar" :url="ratings[2].avatar" />
+          <span>{{ ratings[2].username }}</span>
+          <span class="text-4xl">{{ ratings[2].score }}</span>
         </div>
       </div>
       <div class="list">
-        <div class="user" v-for="i in 14" :key="i">
-          <span class="rating">#{{ leftpad(2, i+3) }}</span>
-          <SokuImgSkeleton class="avatar" url="https://sdfsdf.dev/100x100.png" />
+        <div class="user" v-for="(r, idx) in ratings.slice(3)" :key="r.id">
+          <span class="rating">#{{ leftpad(2, idx+4) }}</span>
+          <SokuImgSkeleton class="avatar" :url="r.avatar" />
           <div class="name-and-rating">
-            <div class="name">{{ faker.name.fullName() }}</div>
-            <div class="rating">{{ Math.random() * 10000 >>> 0 }}</div>
+            <div class="name">{{ r.username }}</div>
+            <div class="rating">{{ r.score }}</div>
           </div>
         </div>
       </div>
+    </div>
+    <div v-else class="-container">
+      <h1 class="text-7xl font-thin text-center">
+        请选择游戏
+      </h1>
     </div>
   </div>
 </template>
