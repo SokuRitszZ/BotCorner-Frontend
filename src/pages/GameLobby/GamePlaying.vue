977b5e08f7f29db93ab2708798b721b38afc72ef
Andrew Leung <1062708758@qq.com>
2023-03-14 22:28:02 +0800

    feat:show true data in game playing view

--- src/pages/GameLobby/GamePlaying.vue
+++ src/pages/GameLobby/GamePlaying.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
 import { getCurrentApi } from '@/api/record';
+import SokuImgSkeleton from '@/components/SokuComponent/SokuSkeleton/SokuImgSkeleton.vue';
 import SokuSkeleton from '@/components/SokuComponent/SokuSkeleton/SokuSkeleton.vue';
-import useCacheStore from '@/store/cacheStore';
+import useCacheStore, { IBot, IUser } from '@/store/cacheStore';
 import { onMounted, ref } from 'vue';
 
 const cacheStore = useCacheStore();
 
-const records = ref<any[]>([]);
+type IRecord = {
+  uuid: string;
+  bots: (IBot | null)[];
+  gameId: number;
+  users: IUser[];
+};
+
+const records = ref<IRecord[]>([]);
 
 onMounted(async () => {
   await cacheStore.getGames;
@@ -20,19 +28,15 @@ onMounted(async () => {
     <h2 class="title">正在进行</h2>
     <template v-if="records.length > 0">
       <main class="game-grid">
-        <SokuSkeleton class="skeleton" :is-loading="false">
+        <SokuSkeleton v-for="record in records" :key="record.uuid" class="skeleton" :is-loading="false">
           <div class="card">
             <h1 class="font-bold text-3xl text-center pt-4">盘蛇</h1>
             <div class="avatars">
-              <img
+              <SokuImgSkeleton
+                v-for="u in record.users"
+                :key="u.id"
                 class="avatar"
-                src="http://localhost:3000/100x100.png"
-                alt=""
-              />
-              <img
-                class="avatar"
-                src="http://localhost:3000/100x100.png"
-                alt=""
+                :url="u.avatar"
               />
             </div>
           </div>
