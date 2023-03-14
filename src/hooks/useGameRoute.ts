974e2cf289c71a83838f1fbdbf69969ff198dc42
Andrew Leung <1062708758@qq.com>
2023-03-14 22:25:02 +0800

    fix:add game route hook

--- src/hooks/useGameRoute.ts
+++ src/hooks/useGameRoute.ts
@@ -1,20 +1,22 @@
 import useCacheStore from "@/store/cacheStore";
 import useRouteStore from "@/store/routeStore";
 import toWord from "@/utils/toWord";
-import { useRouter } from "vue-router";
+import { Router } from "vue-router";
 
-export default async function useGameRoute() {
+export default async function useGameRoute(router: Router) {
   const cacheStore = useCacheStore();
   const routeStore = useRouteStore();
-  const router = useRouter();
 
   const games = await cacheStore.getGames;
   if (!routeStore.hasAddRoute) {
     routeStore.hasAddRoute = true;
-    games.forEach(game => router.addRoute({
-      path: `/game/${game.title}`,
+    games.forEach(game => router.addRoute('game-intro', {
+      path: `/game/intro/${game.id}`,
       name: `game-${game.title}`,
       component: () => import(`@/pages/games/${toWord(game.title)}Game.vue`),
+      meta: {
+        id: game.id,
+      }
     }));
   }
 }
