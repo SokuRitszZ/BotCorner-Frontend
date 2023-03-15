9c9125c9dd08b33439fe067483c5673be58e753a
Andrew Leung <1062708758@qq.com>
2023-03-15 11:53:41 +0800

    feat:refactor gomoku game view

--- src/pages/games/GomokuGame.vue
+++ src/pages/games/GomokuGame.vue
@@ -1,82 +1,66 @@
-<template>
-  <div class="w-full">
-    <div class="w-full flex justify-between items-center">
-      <img
-        class="h-24 rounded-full border-black"
-        :class="isMe(0) && 'border-8'"
-        :src="gameStore.users[0].avatar"
-        alt="avatar"
-      />
-      <div class="text-black font-bold text-5xl">黑子</div>
-    </div>
-  </div>
-  <div class="w-full mt-5">
-    <div class="w-full flex justify-between items-center">
-      <img
-        class="h-24 rounded-full border-white"
-        :class="isMe(1) && 'border-8'"
-        :src="gameStore.users[1].avatar"
-        alt="avatar"
-      />
-      <div class="text-white font-bold text-5xl">白子</div>
-    </div>
-  </div>
-  <div class="mt-5 flex justify-between items-center">
-    <div class="font-bold text-5xl text-gray-400">TURN:</div>
-    <div v-if="turn === 0" class="text-black font-bold text-5xl">黑子</div>
-    <div v-else-if="turn === 1" class="text-white font-bold text-5xl">白子</div>
-  </div>
-</template>
-
 <script setup lang="ts">
+import SokuImgSkeleton from '@/components/SokuComponent/SokuSkeleton/SokuImgSkeleton.vue';
+import useBindEvent from '@/hooks/useBindEvent';
 import useGameStore from '@/store/gameStore';
+import useMatchStore from '@/store/matchStore';
 import useUserStore from '@/store/userStore';
-import GameWebSocket from '@/utils/GameWebSocket';
 import { onMounted, ref } from 'vue';
 
 const userStore = useUserStore();
 const gameStore = useGameStore();
 const cnt = ref<number[]>([0, 0]);
-const turn = ref<number>(-1);
+const turn = ref<number>(0);
+const flag = ['黑方', '白方'];
 
-type PropsType = {
-  promise_server: Promise<GameWebSocket>;
-};
-const props = defineProps<PropsType>();
+useBindEvent('set step truly', () => {
+  turn.value ^= 1;
+});
 
-const isMe = (id: number) => {
-  return gameStore.users.findIndex((user) => user.id === userStore.id) === id;
-};
+const matchStore = useMatchStore();
 
 onMounted(async () => {
-  const server = (await props.promise_server).on({
-    action: 'set step truly',
-    callback: () => {
-      turn.value ^= 1;
-    },
+  const server = matchStore.server!;
+
+  gameStore.clearEvents();
+  
+  gameStore.on('prepare', () => {
+    turn.value = 0;
+    cnt.value = [0, 0];
   });
-  gameStore
-    .on('prepare', () => {
-      turn.value = 0;
-      cnt.value = [0, 0];
-    })
-    .on('click', (data: any) => {
-      if (gameStore.game!.mode === 'single') {
-        server.sendMessage({
-          action: 'set step',
-          data,
-        });
-      } else if (gameStore.game!.mode === 'multi') {
-        server.sendMessage({
-          action: 'set step',
-          data: {
-            ...data,
-            id: gameStore.users.findIndex((user) => user.id === userStore.id),
-          },
-        });
-      }
-    });
+  gameStore.on('click', (data: any) => {
+    if (gameStore.game!.mode === 'single') {
+      server.sendMessage('set step', data);
+    } else if (gameStore.game!.mode === 'multi') {
+      server.sendMessage('set step', {
+        ...data,
+        id: matchStore.getIndex(userStore.id),
+      });
+    }
+  });
 });
 </script>
+
+<template>
+ <div class="w-full flex justify-around items-center relative">
+    <div v-for="i in 2" :key="i" class="flex flex-col items-center">
+      <span
+        class="text-3xl font-bold"
+        :class="[(i - 1 && 'text-[#ccc]') || '']"
+        >{{ flag[i - 1] }}</span
+      >
+      <SokuImgSkeleton
+        class="w-[100px] h-[100px]"
+        :url="matchStore.usersMatch[i - 1].avatar"
+      />
+      <span class="text-xl font-thin">{{ matchStore.usersMatch[i - 1].username }}</span>
+    </div>
+    <div
+      class="text-6xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold"
+      :class="[(turn && 'text-[#ccc]') || '']"
+    >
+      {{ flag[turn] }}
+    </div>
+  </div>
+</template>
 
 <style scoped lang="scss"></style>
