5be389a4f96149f72e574c43ca105967f89ecf16
Andrew Leung <1062708758@qq.com>
2023-03-15 18:11:33 +0800

    optimize:use throttle in refresh get playing

--- src/pages/GameLobby/GamePlaying.vue
+++ src/pages/GameLobby/GamePlaying.vue
@@ -2,6 +2,7 @@
 import { getCurrentApi } from '@/api/record';
 import useCacheStore, { IBot } from '@/store/cacheStore';
 import { IUser } from '@/store/userStore';
+import { throttle } from 'lodash';
 import { onMounted, ref } from 'vue';
 import PlayingCard from '../games/PlayingCard.vue';
 
@@ -21,11 +22,24 @@ onMounted(async () => {
   const result = await getCurrentApi();
   records.value = (result as any).results;
 });
+
+async function _refresh() {
+  records.value = ((await getCurrentApi()) as any).results;
+}
+
+const refresh = throttle(_refresh, 1000);
 </script>
 
 <template>
   <div class="game-playing">
-    <h2 class="title">正在进行</h2>
+    <h2 class="title flex items-center justify-between">
+      <span>
+        正在进行
+      </span>
+      <button @click="refresh" class="text-sm bg-gray-300 px-2 rounded-sm">
+        刷新
+      </button>
+    </h2>
     <template v-if="records.length > 0">
       <main class="game-grid">
         <PlayingCard v-for="record in records" :key="record.uuid" :record="record" />
